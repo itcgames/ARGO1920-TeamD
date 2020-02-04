42296a18e791cadceeae70572290a12e9e6cdf5d
Keith <c00205321@itcarlow.ie>
2020-02-04 16:52:10 +0000

    Created game loop and main

---
+++ ARGO/ARGO/Game.cpp
@@ -0,0 +1,72 @@
+#include "Game.h"
+
+Game::Game()
+{
+
+}
+
+Game::~Game()
+{
+
+}
+
+void Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
+{
+	int flags = 0;
+	if (fullscreen)
+	{
+		flags = SDL_WINDOW_FULLSCREEN;
+	}
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
+		m_window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+
+		m_renderer = SDL_CreateRenderer(m_window, -1, 0);
+		if (m_renderer)
+		{
+			SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 255);
+		}
+
+		isRunning = true;
+	}
+}
+
+void Game::handleEvents()
+{
+	SDL_PollEvent(&m_event);
+	switch (m_event.type)
+	{
+	case SDL_QUIT:
+		isRunning = false;
+		break;
+	case SDL_KEYDOWN:
+		if (m_event.key.keysym.sym == SDLK_ESCAPE)
+		{
+			isRunning = false;
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+void Game::update()
+{
+	static int count = 0; count++;
+	std::cout << count << std::endl;
+}
+
+void Game::render()
+{
+	SDL_RenderClear(m_renderer);
+
+	SDL_RenderPresent(m_renderer);
+}
+
+void Game::clean()
+{
+	SDL_DestroyWindow(m_window);
+	SDL_DestroyRenderer(m_renderer);
+	SDL_Quit();
+	std::cout << "Game Cleaned" << std::endl;
+}
---
+++ ARGO/ARGO/Game.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+
+	bool running() { return isRunning; }
+
+
+	SDL_Renderer* m_renderer;
+	SDL_Event m_event;
+private:
+	bool isRunning;
+	SDL_Window* m_window;
+
+};

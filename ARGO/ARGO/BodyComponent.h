0cbbb801de3a29ab8136369f4856ea3ade1bb69e
Quintin Furlong <c00218453@itcarlow.ie>
2020-02-07 09:16:22 +0000

    default construster for body

--- ARGO/ARGO/BodyComponent.h
+++ ARGO/ARGO/BodyComponent.h
@@ -5,11 +5,14 @@
 class BodyComponent : public Component
 {
 public:
+	BodyComponent();
+	~BodyComponent();
 	BodyComponent(Vector2 size);
 	Vector2 getSize();
 	void setSize(Vector2 size);
 	void init() override;
 	void update() override;
+	void render() override;
 private:
 	Vector2 m_size;
 };

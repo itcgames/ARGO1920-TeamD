52cf6dcd5bd37b70b786a5c23dbc0301f033fc77
Dylan Curran <c00217112@itcarlow.ie>
2020-02-11 12:59:34 +0000

    added a collision System for game
    Takes in two body components and two position components as arguments detected for the collision

---
+++ ARGO/ARGO/CollisionSystem.h
@@ -0,0 +1,17 @@
+#ifndef COLLISIONSYSTEM
+#define COLLISIONSYSTEM
+#include <string>
+#include "PositionComponent.h"
+#include "BodyComponent.h"
+class CollisionSystem
+{
+
+public:
+	CollisionSystem();
+	~CollisionSystem();
+	bool collides(PositionComponent & t_pos1, BodyComponent& t_body1, PositionComponent & t_pos2, BodyComponent & t_body2);
+private:
+	bool m_isColliding;
+};
+#endif	// !COLLISIONSYSTEM
+

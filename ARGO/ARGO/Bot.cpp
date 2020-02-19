afb4711a7676e086181e35bea78eb893519972a3
Martin <c00157047@itcarlow.ie>
2020-02-19 09:24:17 +0000

    bot class

---
+++ ARGO/ARGO/Bot.cpp
@@ -0,0 +1,31 @@
+#include"Bot.h"
+
+Bot::Bot()
+{
+}
+
+Bot::~Bot()
+{
+}
+
+Vector2 Bot::getPos()
+{
+	return m_pos;
+}
+
+Vector2 Bot::getVelocity()
+{
+	return m_vel;
+}
+
+void Bot::move()
+{
+}
+
+void Bot::collisionDetection()
+{
+}
+
+void Bot::boundaryDetection()
+{
+}

319cb29820407f6fca3ca54731bacdcefd2deda6
GregCahillC00216328 <gregcahill17@gmail.com>
2020-02-26 14:59:08 +0000

    Updated art to remove death

--- ARGO/ARGO/Game.cpp
+++ ARGO/ARGO/Game.cpp
@@ -1,6 +1,6 @@
 #include "Game.h"
 
-GameState Game::m_currentMode{ GameState::splash };
+GameState Game::m_currentMode{ GameState::gameplay };
 
 EntityManager manager;
 auto& newPlayer(manager.addEntity("player"));
